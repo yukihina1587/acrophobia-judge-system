c041e9af921f9817fea148a60fb5f2ee9ba0fb9a
yukihina1587 <iku.k625@gmail.com>
2019-07-12 17:38:20 +0900

    Arduino(C)→PC(Python)にデータをシリアル通信するプログラムを実装した

--- Arduino/Get-EFG.py
+++ Arduino/Get-EFG.py
@@ -2,15 +2,20 @@
 import serial
 import re
 def main():
-    with serial.Serial('COM6',9600,timeout=1) as ser:
+    with serial.Serial('COM6',115200,timeout=1) as ser:
 
         while True:
-            c = ser.readline()
-            d = re.findall('[0-9]+\.+[0-9]',str(c),flags=0)
-            d = [float(i) for i in d]
-            for i in range(0, len(d)):  #要素を1つずつ順番に出力します
-                print(d[i])
-            print
+            String_data = ser.read()
+            int_data = int.from_bytes(String_data, 'big')
+            print(int_data)
+
+        #while True:
+            #c = ser.readline()
+            #d = re.findall('[0-9]+\.+[0-9]',str(c),flags=0)
+            #d = [float(i) for i in d]
+            #for i in range(0, len(d)):  #要素を1つずつ順番に出力します
+                #print(d[i])
+            #print
         ser.close()
 
 if __name__=="__main__":
