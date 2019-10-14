d188d203756da8c4933eff65cb34aa343cd7bd73
yukihina1587 <iku.k625@gmail.com>
2019-10-14 15:16:07 +0900

    脳波測定機能

--- MindWaveMobile2/Get-EEG.py
+++ MindWaveMobile2/Get-EEG.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
 import thinkgear
 
 PORT = 'COM5'
+
+attention = 0
+meditation = 0
 
 for packets in thinkgear.ThinkGearProtocol(PORT).get_packets():
     for pkt in packets:
@@ -10,7 +14,17 @@ for packets in thinkgear.ThinkGearProtocol(PORT).get_packets():
         t = str(pkt)
 
         if t != '':
-            differencer = t[0:1] # 最初の文字を取り出す
-            if int(differencer) == 1:
-                attention = t[1:]  # 最初の文字以外をattentionとして代入
-                print(attention)
+            print(t[0:1])
+            #differencer = ''
+            #differencer = t[0:1] # 最初の文字を取り出す
+            #if int(differencer) == 1:
+            #    attention = t[1:]  # 最初の文字以外をattentionとして代入
+            #    print(attention)
+
+        if int(attention) == 0:  # or int(meditation) == 0:
+            continue
+        print "attention", attention
+        # print "meditation", meditation
+
+        attention = int(attention)
+        # meditation = float(meditation)
